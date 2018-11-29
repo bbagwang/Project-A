14a3db0b9d2213e617a9377db89707628ca4d83d
JangWoo <ljh960101@gmail.com>
2018-11-29 22:54:53 +0900

    C++ 모듈 추가, MonsterBase 구현
    AMonsterBase 클래스엔 기획 내용에 맞춰 추가해야합니다.
    현재는 Hp 시스템만 있습니다.

---
+++ Source/Project_A/Monster/MonsterBase.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MonsterBase.generated.h"
+
+UCLASS()
+class PROJECT_A_API AMonsterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+// Fields
+public:
+protected:
+	float _maxHp = 100;
+	float _hp;
+private:
+
+// Methods
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void Die();
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, class AActor* DamageCauser) override;
+
+public:
+	// Sets default values for this character's properties
+	AMonsterBase();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	// Apply Damage. Return val is remain hp.
+	float GetDamage(float damage);
+	float GetHp() { return _hp; }
+};
